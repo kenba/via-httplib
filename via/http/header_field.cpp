3fc012ddca30aed94adf0381ffa3426ffaca91e3
Ken Barker <ken.barker@via-technology.co.uk>
2014-02-18 19:31:17 +0000

    Prepare for 0.16 tag

--- via/http/header_field.cpp
+++ via/http/header_field.cpp
@@ -16,7 +16,7 @@
 namespace
 {
   /// The server name for the server header.
-  const std::string	SERVER_NAME             ("Via-httplib/0.15");
+  const std::string	SERVER_NAME             ("Via-httplib/0.16");
 
   /// The message/http content for a TRACE response
   const std::string	MESSAGE_HTTP            ("message/http");
