80db82e2010c3ba0484e1d5ca1cb04eb01a6af44
Ken Barker <ken.barker@via-technology.co.uk>
2014-03-03 22:35:05 +0000

    Add RX_LENGTH_REQUIRED state.

--- via/http/headers.hpp
+++ via/http/headers.hpp
@@ -31,6 +31,7 @@ namespace via
     enum receiver_parsing_state
     {
       RX_INVALID,         ///< the message is invalid
+      RX_LENGTH_REQUIRED, ///< the message requires a content-length header
       RX_EXPECT_CONTINUE, ///< the client expects a 100 Continue response
       RX_INCOMPLETE,      ///< the message requires more data
       RX_VALID,           ///< a valid request or response
